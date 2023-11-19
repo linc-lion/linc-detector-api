e53ec8adb63e6035997747671ac85336b31860ab
Habiba Mohamed <habibam314@gmail.com>
2023-11-18 21:18:35 -0800

    update output type and add logger

--- src/linc_detector.py
+++ src/linc_detector.py
@@ -1,16 +1,26 @@
 from io import BytesIO
-from typing import Any, Dict
+from typing import Any, Dict, Union
+import loggerFactory
 import glob
 import os
+import json
 import bentoml
-from bentoml.io import JSON, Image
+from bentoml.io import JSON, Image, Multipart
 from PIL.Image import Image as PILImage
+from flask import jsonify
 from numpy.typing import NDArray
 from werkzeug.utils import secure_filename
 from utils.predictor import predict
+import numpy as np
+import torchvision
+import base64
 
 bento_model = bentoml.pytorch.get("habiba:latest")
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+logger = loggerFactory.StreamOnlyLogger(rand_name=True)
+
+img_json_output_spec = Multipart(input_image=Image(), bounding_box_coords=JSON())
 
 
 class LincDetectionRunnable(bentoml.Runnable):
@@ -19,12 +29,12 @@ class LincDetectionRunnable(bentoml.Runnable):
 
     def __init__(self):
         # load the model instance
-        self.classifier = bentoml.pytorch.load_model(bento_model)
+        self.detector = bentoml.pytorch.load_model(bento_model)
 
     @bentoml.Runnable.method(batchable=False)
-    def is_spam(self, input_data: NDArray[Any]) -> NDArray[Any]:
+    def has_lion(self, input_data: NDArray[Any]) -> NDArray[Any]:
 
-        return self.classifier[(input_data)]
+        return self.detector[(input_data)]
 
 
 linc_detector_runner = bentoml.Runner(LincDetectionRunnable, models=[bento_model])
@@ -32,43 +42,41 @@ svc = bentoml.Service("linc_detector", runners=[linc_detector_runner])
 print(svc)
 
 
-@svc.api(input=Image(), output=JSON(), route='/v1/annotate')
-def detect(image: PILImage, ctx: bentoml.Context) -> Dict[str, Any]:
-    headers = ctx.request.headers
+@svc.api(input=Image(), output=img_json_output_spec, route='/v1/annotate')
+def detect(image: PILImage, ctx: bentoml.Context) -> Union[Dict[str, str], Dict[str, str], Dict[str, str], str]:
+    try:
+        clear_old_files()
 
-    # Check if 'content-type' key is present in headers
-    content_type = headers.get('content-type')
+        headers = ctx.request.headers
 
-    if content_type is None:
-        ctx.response.status_code = 405
-        return {'error': 'Content-Type header is missing'}
+        # Check if 'content-type' key is present in headers
+        content_type = headers.get('content-type')
 
-    if not image:
-        return {'error': 'No file sent'}
+        if content_type is None:
+            ctx.response.status_code = 405
+            return {'error': 'Content-Type header is missing'}
 
-    clear_old_files()
-    query_params = ctx.request.query_params
-    if 'vert_size' in ctx.request.query_params:
-        vert_size = int(ctx.request.query_params.get('vert_size'))
-    else:
-        vert_size = 500
-    print(vert_size)
-    print("vert_size")
-    print(allowed_file(image.format))
-    print("allowed_file(image.format)")
-    if not allowed_file(image.format):
+        if not image:
+            return {'error': 'No file sent'}
+
+        query_params = ctx.request.query_params
+        if 'vert_size' in ctx.request.query_params:
+            vert_size = int(ctx.request.query_params.get('vert_size'))
+        else:
+            vert_size = 500
+        if not allowed_file(image.format):
+            ctx.response.status_code = 500
+            allowed_types_error = {'error': 'Allowed image types are -> png, jpg, jpeg'}
+            return allowed_types_error
+        output = process_uploaded_file(image, vert_size)
+
+        image_with_boxes, bounding_box_coords = output
+
+        return {'input_image': image, 'bounding_box_coords': bounding_box_coords}
+
+    except Exception:
         ctx.response.status_code = 500
-        allowed_types_error = {'error': 'Allowed image types are -> png, jpg, jpeg'}
-        return allowed_types_error
-    habiba = process_uploaded_file(image, vert_size, ctx)
-    print(habiba)
-    print("habiba")
-    predicted_picture_output, bounding_box_coords = habiba
-
-    print(query_params)
-    print("query_params")
-
-    return {'input_image': predicted_picture_output, 'bounding_box_coords': bounding_box_coords}
+        logger.error("Exception in predict method!")
 
 
 def clear_old_files():
@@ -78,42 +86,16 @@ def clear_old_files():
         os.remove(f)
 
 
-def process_uploaded_file(file, vert_size, ctx):
-    # buffer = BytesIO()
-    # input.save(file, format=input.format)
-
-    # filename = 'habiba'
-    # print(filename)
-    # print("filename")
-    # input_image_path = os.path.join('upload_folder/')
-    # output_image_path = os.path.join('upload_folder/', 'annotated_')
-
-    # print(input_image_path)
-    print("input_image_path")
-    print(file.format)
-    file.save("input_image_path", file.format)
+def process_uploaded_file(file, vert_size):
+    file.save("input_image_path", 'png')
 
     predicted_picture_output = predict("input_image_path", vert_size=vert_size)
     bounding_box_coords = predicted_picture_output['box_coordinates']
-    return predicted_picture_output, bounding_box_coords
+    image_with_boxes = predicted_picture_output["image_with_boxes"]
+    return image_with_boxes, bounding_box_coords
 
 
 def allowed_file(filename):
     print(filename)
     print("filename in allowed files")
     return filename.lower() in ALLOWED_EXTENSIONS
-
-    return input_image_path, bounding_box_coords
-# print(f"Running inference on {device} device")
-#
-# print('Loading image... ', end='', flush=True)
-# loaded_image = PIL.Image.open(image_path)
-# width, height = loaded_image.size
-# print(f'Input image_width: {width}, image_height: {height}')
-# image = to_tensor(loaded_image).to(device)
-# print('Done.')
-#
-# label_names = checkpoint['label_names']
-#
-# print('Running image through model... ', end='', flush=True)
-# tic = time.time()
