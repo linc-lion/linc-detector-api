bc3984f04365003aabfc0345bc12bde675f22ae6
Habiba Mohamed <habibam314@gmail.com>
2023-11-19 20:45:10 -0800

    refactor to use has_lion for predict method

--- src/linc_detection.py
+++ src/linc_detection.py
@@ -28,14 +28,19 @@ class LincDetectionRunnable(bentoml.Runnable):
     SUPPORTS_CPU_MULTI_THREADING = True
 
     def __init__(self):
-        # load the model instance
-        self.detector = bentoml.pytorch.load_model(bento_model)
+        # No need to load the model here, as the model is loaded in predictor.py
+        pass
 
     @bentoml.Runnable.method(batchable=False)
     def has_lion(self, input_data: NDArray[Any]) -> NDArray[Any]:
+        # Assuming input_data is an image, convert it to a PIL Image
+        # input_image = PIL.Image.open(input_data)
 
-        return self.detector[input_data]
+        # Call the predict method from predictor.py
+        prediction_result = predict(input_data, vert_size=500)
 
+        # Return the result, adjust as needed based on the actual output of predict method
+        return prediction_result
 
 linc_detector_runner = bentoml.Runner(LincDetectionRunnable, models=[bento_model])
 svc = bentoml.Service("linc_detector", runners=[linc_detector_runner])
@@ -44,39 +49,39 @@ print(svc)
 
 @svc.api(input=Image(), output=JSON(), route='/v1/annotate')
 def detect(image: PILImage, ctx: bentoml.Context) -> Union[Dict[str, str], Dict[str, str], Dict[str, str], str]:
-    try:
-        clear_old_files()
+    # try:
+    clear_old_files()
 
-        headers = ctx.request.headers
+    headers = ctx.request.headers
 
-        # Check if 'content-type' key is present in headers
-        content_type = headers.get('content-type')
+    # Check if 'content-type' key is present in headers
+    content_type = headers.get('content-type')
 
-        if content_type is None:
-            ctx.response.status_code = 405
-            return {'error': 'Content-Type header is missing'}
+    if content_type is None:
+        ctx.response.status_code = 405
+        return {'error': 'Content-Type header is missing'}
 
-        if not image:
-            return {'error': 'No file sent'}
+    if not image:
+        return {'error': 'No file sent'}
 
-        query_params = ctx.request.query_params
-        if 'vert_size' in query_params:
-            vert_size = int(query_params.get('vert_size'))
-        else:
-            vert_size = 500
-        if not allowed_file(image.format):
-            ctx.response.status_code = 500
-            allowed_types_error = {'error': 'Allowed image types are -> png, jpg, jpeg'}
-            return allowed_types_error
-        output = process_uploaded_file(image, vert_size)
-
-        bounding_box_coords = output
-
-        return {'bounding_box_coords': bounding_box_coords}
-
-    except Exception:
+    query_params = ctx.request.query_params
+    if 'vert_size' in query_params:
+        vert_size = int(query_params.get('vert_size'))
+    else:
+        vert_size = 500
+    if not allowed_file(image.format):
         ctx.response.status_code = 500
-        logger.error("Exception in predict method!")
+        allowed_types_error = {'error': 'Allowed image types are -> png, jpg, jpeg'}
+        return allowed_types_error
+    output = process_uploaded_file(image, vert_size)
+
+    bounding_box_coords = output
+
+    return {'bounding_box_coords': bounding_box_coords}
+
+    # except Exception:
+    #     ctx.response.status_code = 500
+    #     logger.error("Exception in predict method!")
 
 
 def clear_old_files():
@@ -89,8 +94,15 @@ def clear_old_files():
 def process_uploaded_file(file, vert_size):
     file.save("input_image_path", 'png')
 
-    predicted_picture_output = predict("input_image_path", vert_size=vert_size)
-    bounding_box_coords = predicted_picture_output['box_coordinates']
+    # Create an instance of LincDetectionRunnable
+    linc_detector_instance = LincDetectionRunnable()
+
+    # Call has_lion method to get predictions
+    prediction_result = linc_detector_instance.has_lion("input_image_path")
+
+    # Assuming prediction_result is a dictionary containing the bounding box coordinates
+    bounding_box_coords = prediction_result.get("box_coordinates", None)
+
     return bounding_box_coords
 
 
